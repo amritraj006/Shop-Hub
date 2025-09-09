81e39201392714a36573ccd94bdb7b94765465ad
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2025-09-09 08:35:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://shop-hub-xm7o.onrender.com', // backend running on port 3000
+        target: 'http://localhost:3000', // backend running on port 3000
         changeOrigin: true,
         secure: false,
       },
